b15a05b2b9b9fdfea3b09df0410c6b6b28e07244
syedusamaazhar89 <usamaazhar89.ua@gmail.com>
2020-06-14 13:45:44 +0500

    JavaScriptAssignment5

---
+++ app.js
@@ -0,0 +1,446 @@
+// // Chapter 01
+// // Task-01
+
+// // Ans
+
+// alert("Welcome To Our Website");
+
+// // Task-02
+
+// // Ans
+
+// alert("Error! Please enter a valid password.");
+
+
+// // Task-03
+
+// // Ans
+
+
+// alert("Welcome to JS Land..." + "\n" + "Happy Coding!");
+
+
+// // Task-04
+
+// // Ans
+
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+
+
+
+// // Task-05
+
+// // Ans
+
+// // Done it in console
+
+
+// // Task-06
+
+// // Ans
+
+// // Done it with last website project
+
+
+// // Task-07
+
+// // Ans
+
+// // Done it
+
+
+
+
+// // Chapter 02
+// // Task-01
+
+// // Ans
+
+// var username;
+
+// // Task-02
+
+// // Ans
+
+// var myname;
+
+// myname = "Syed Usama Azhar";
+
+
+// //Task-03
+
+// // Ans
+
+// var titledmessage;
+// titledmessage = "Hello World";
+// alert(titledmessage);
+
+
+// // Task-04
+
+// // Ans
+
+
+// var name = myname;
+// var age = "30 years old";
+// var city = "Karachi"
+// var course = "Mobile and Web Development";
+// alert(name);
+// alert(age);
+// alert(city);
+// alert(course);
+
+
+// // Task-05
+
+// // Ans
+
+// var p = "PIZZA";
+// alert(p + "\n" + p.slice(0,4) + "\n" + p.slice(0,3) + "\n" + p.slice(0,2) + "\n" + p.slice(0,1));
+
+
+
+// // Task-06
+// // Ans
+
+// var  email;
+// email = "usamaazhar89.ua@gmail.com";
+// alert("My email address is" + " " + email);
+
+
+
+// // Task-07
+
+// // Ans
+
+// var  book;
+// book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book" + " " + book);
+
+
+// // Task-08
+
+// // Ans
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+
+
+// // Task-09
+
+// // Ans
+
+// var a = "▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬";
+// alert(a);
+
+
+
+// // Chapter 03
+
+// // Task-01
+
+// // Ans
+
+// var age = 30;
+// alert("I am"+ " " + age  + " " + "years old");
+
+
+// // Chapter 03
+
+// // Task-02
+
+// // Ans
+
+// var a = [];
+// for(var a = 1; a<= 20; a++) {
+//     alert(`You have visited this site ${a} times`);
+
+// }
+
+
+// // Chapter 03
+
+// // Task-03
+
+// // Ans
+
+// var birthYear = 1989;
+// document.write("My Birth Year is" + " " +birthYear);
+
+
+// // Chapter 03
+
+// // Task-04
+
+// // Ans
+
+
+// var name = "John Doe";
+// var product = "T-Shirts";
+// var Quantity = "5";
+
+// var b = name.bold();
+
+// document.write(name.bold() + " " +"ordered" + " " + Quantity.bold() + " " + product.bold() + "(s)" + " " + "on XYZ Clothing Store");
+
+
+// // Chapter 04
+
+// // Task-01
+
+// // Ans
+
+// var a,b,c;
+
+
+// // Chapter 04
+
+// // Task-02
+
+// // Ans
+
+// var 
+// name, 
+// _age, 
+// $city, 
+// userName, 
+// full_name;
+
+// var 
+// 1name, 
+// na me, 
+// var, 
+// -name,
+// const
+// ;
+
+
+// // Chapter 04
+
+// // Task-03
+
+// // Ans
+
+// var vh = "Rules for naming JS Variables";
+
+// var va = "Variable names can only contain letters, numbers, $ and _ . For example $my_1stVariable";
+
+// var vb = "Variable must begin with a letters, $ or _ . For example $name, _name or name";
+
+// var vc = "Variable names are case sensitive";
+
+// var vd = "Variable names should not be JS keywords";
+
+// document.write(vh.bold() + "<br> <br> <br>" + va + "<br>" + vb + "<br>" + vc + "<br>" + vd);
+
+
+// // // Chapter 05
+
+// // // Task-01
+
+// // // Ans
+
+// var first = +prompt("Enter Your first num");
+// var second = +prompt("Enter Your first num");
+// var sum = first + second;
+// document.write("Sum of" + " " + first + " " + "+" + " " +second + " " + "is" + " " + sum);
+
+
+// // // Chapter 05
+
+// // // Task-02
+
+// // // Ans
+
+// var first = prompt("Enter Your first num");
+// var second = prompt("Enter Your first num");
+// var sub = first - second;
+// document.write("Substraction of" + " " + first + " " + "-" + " " +second + " " + "is" + " " + sub);
+
+// var first = prompt("Enter Your first num");
+// var second = prompt("Enter Your first num");
+// var mul = first * second;
+// document.write("Multiplication of" + " " + first + " " + "x" + " " +second + " " + "is" + " " + mul);
+
+// var first = prompt("Enter Your first num");
+// var second = prompt("Enter Your first num");
+// var div = first / second;
+// document.write("Division of" + " " + first + " " + "/" + " " +second + " " + "is" + " " + div);
+
+// var first = prompt("Enter Your first num");
+// var second = prompt("Enter Your first num");
+// var mod = first % second;
+// document.write("Modulus of" + " " + first + " " + "%" + " " +second + " " + "is" + " " + mod);
+
+
+// // // Chapter 05
+
+// // // Task-03
+
+// // // Ans
+
+// var num;
+// document.write("Value after variable declaration is " + " " + num);
+// num = 10;
+// document.write("<br>" + "Initial Value: " + " " + num);
+// num = ++num;
+// document.write("<br>" + "Value after increment is: " + " " + num);
+// num = num+7;
+// document.write("<br>" + "Value after addition is: " + " " + num);
+// num = --num;
+// document.write("<br>" + "Value after decrement is: " + " " + num);
+// num = num % 3;
+// document.write("<br>" + "The remainder is: " + " " + num);
+
+
+// // // Chapter 05
+
+// // // Task-04
+
+// // // Ans
+
+// var tp = 600;
+// var tickets = prompt("Pleas Enter!\n How many Tickets you want to buy");
+// var totalCost = tp * tickets;
+// document.write(`Total cost to buy ${tickets.bold()} tickets to a movie is ${totalCost} PKR`);
+
+
+// // // Chapter 05
+
+// // // Task-05
+
+// // // Ans
+
+// document.write("Table of 10" + "<br> <br>");
+// for(i= 1; i <=10; i++){
+//     document.write("10" + " " + "x" + " " + i + " " + "=" + " " + 10*i + "<br>");
+// }
+
+
+// // // Chapter 05
+
+// // // Task-06
+
+// // // Ans
+
+// var c = 38;
+// var ctoF = (c*9)/5 + 32;
+// var f = 110;
+// var ftoC = ((f - 32)*5)/9;
+// document.write(c + " " + '°C is ' + ctoF + " " + '°F' + "<br>",f + " " + '°F is ' + ftoC + " " + '°C');
+
+
+// // // Chapter 05
+
+// // // Task-07
+
+// // // Ans
+
+
+// var pI1 = 800;
+// var pI2 = 100;
+// var QI1 = 7;
+// var QI2 = 3;
+// var Ship = 100;
+// var tc = pI1 * QI1 + pI2 * QI2 + Ship;
+
+// document.write("<h1> Shopping Cart </h1>" + "<br>" + "<br>");
+// document.write("Price of item 1 is" + " " + pI1 + "<br>");
+// document.write("Quantity of item 1 is" + " " + QI1 + "<br>");
+// document.write("Price of item 2 is" + " " + pI2 + "<br>");
+// document.write("Quantity of item 2 is" + " " + QI2 + "<br>");
+// document.write("Shipping Charges is" + " " + Ship + "<br>" + "<br>");
+// document.write("Total cost of your order is" + " " + tc);
+
+
+
+// // // Chapter 05
+
+// // // Task-08
+
+// // // Ans
+
+// var tm = 3400;
+// var mo = 2495;
+// var per = mo/tm*100;
+
+// document.write("<h1> Marks Sheet </h1>" + "<br>" + "<br>")
+// document.write("Total Marks = " + tm + "<br>", "Marks Obtained = " + mo + "<br>", "Percentage = " + per + " % ");
+
+
+
+// // // Chapter 05
+
+// // // Task-09
+
+// // // Ans
+
+// var usdollar = 100;
+// var saudiriyal = 100;
+// var totalcurreny = (saudiriyal*28) + (usdollar*104.80);
+// document.write("<h1> Currency in PKR </h1>" + "<br>" + "<br>");
+// document.write("Total Currency in PKR = " + totalcurreny);
+
+
+
+// // // // Chapter 05
+
+// // // // Task-10
+
+// // // // Ans
+
+// var number = 22;
+// var total = ((number + 5) * 10 / 2);
+// document.write(total);
+
+
+
+// // // // Chapter 05
+
+// // // // Task-11
+
+// // // // Ans
+
+// var currentyear = 2020;
+// var birthyear = 1989;
+// var age = currentyear - birthyear;
+// document.write("<h1>Age Calculator</h1>" + "<br>" + "<br>");
+// document.write("Current Year = " + currentyear + "<br>", "Birth Year = " + birthyear + "<br>", "Your Age is = " + age);
+
+
+
+// // // // Chapter 05
+
+// // // // Task-12
+
+// // // // Ans
+
+// var radius = 20;
+// var circumference = Math.PI * 2*radius;
+// var area = Math.PI * radius*radius;
+// document.write("<h1>The Geometrizer</h1>" + "<br>" + "<br>");
+// document.write("Radius of a circle = " + " " + radius + "<br>","The circumference is = " + " " + circumference + "<br>","The area is = " + " " + area);
+
+
+// // // // Chapter 05
+
+// // // // Task-13
+
+// // // // Ans
+
+var favsnk = "Samosa";
+var age = 30;
+var maxAge = 70;
+var numPerDay = 3;
+var totalNeeded = (numPerDay * 365) * (maxAge - age);
+var message = 'You will need ' + totalNeeded + " " + favsnk +  ' to last you until the ripe old age of ' + " " + maxAge;
+document.write("<h1>The Lifetime Supply Calculator</h1>" + "<br>" + "<br>");
+document.write("Your Favorite Snack = " + favsnk + "<br>",
+                 "Current age = " + age + "<br>",
+                 "Estimated Max Age = " + maxAge + "<br>", 
+                 "Amount of snacks per day = " + numPerDay + "<br>",
+                  message);
+
